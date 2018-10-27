9defa58c50a34982936bdcb2b8af932f0aabd5b8
Sleightly <ejc042@ucsd.edu>
2018-10-27 11:44:22 -0700

    updated server

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ var http = require('http'),
     express = require('express'),
     path = require('path');
 
+const PORT = process.env.PORT || 1500
+
 var app = express();
 
 app.use(express.static(__dirname));
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+"/public/", 'index.html'));
+  res.sendFile(path.join(__dirname+"/public/views", 'index.html'));
 })
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-
-var server = http.createServer(app);
-
-server.listen(1500);
