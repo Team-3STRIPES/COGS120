613bdd070ba4dc9067a42df42bfe6de6f3a55288
Nathaniel Qin <nathaniel.qin1@gmail.com>
2018-11-08 21:34:23 -0800

    integrated some modals

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,22 +1,27 @@
-var X = $(".close")[0];
+var isOpen = false;
 
 function render_modal(modal_id, header_id, body_id, h_desc, b_desc) {
-	var $modal = $('#'+modal_id)[0];
-	var $header = $('#'+header_id)[0];
-	var $body = $('#'+body_id)[0];
-	$modal.style.display = "block";
-	$header.childNodes[0].nodeValue = h_desc;
-	$body.childNodes[0].nodeValue = b_desc;
-}
-
-X.onclick = function() {
-    modal.style.display = "none";
+	var $modal = $('#'+modal_id);
+	var $header = $('#'+header_id);
+	var $body = $('#'+body_id);
+	$modal.css("display", "block");
+	$header.text(h_desc);
+	$body.text(b_desc);
+  setTimeout(function() {
+    isOpen = true;
+  }, 0);
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-	var modal = $(".modal");
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+$(window).click(function(e) {
+  if (($(e.target).parents('.modal-content').length === 0 && isOpen) ||
+      ($(e.target).hasClass('close'))) {
+    closeModal();
+  }
+})
+
+function closeModal() {
+  var $modal = $(".modal");
+  $modal.css("display", "none");
+  isOpen = false;
 }
