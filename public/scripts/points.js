2e5dacd86de468a87ae8c32aae288b183b616080
Nathaniel Qin <nathaniel.qin1@gmail.com>
2018-12-02 18:33:25 -0800

    shop buttons properly change now

--- public/scripts/points.js
+++ public/scripts/points.js
@@ -503,7 +503,7 @@ function updateShopButtons() {
     success: function(data, textStatus, jqXHR) {
       var themes = data.themes;
       for(var i = 0; i < themes.length; i++) {
-        console.log($(`#${themes[i]}`).last());
+        $(`#${themes[i]} .item-buy`).text('Use);
       }
     }
   });
